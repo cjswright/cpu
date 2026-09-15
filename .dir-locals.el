8216801c4d52cb77a2173ca9ce0c678b5809fa15
Christian Wright <cjswright00@gmail.com>
2026-09-15 21:51:56 +1200

    Setup LSP

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,5 +3,8 @@
 ;;;     M-x describe-variable RET dir-locals-class-alist RET
 
 ((verilog-mode
-  . ((lsp-clients-svlangserver-includeIndexing . ["src/**/*.{sv,svh}"])
-     (lsp-clients-svlangserver-launchConfiguration . "verilator -sv --lint-only -Wall --timing"))))
+  . ((lsp-clients-svlangserver-includeIndexing . ["**/*.{sv,svh}"])
+     (lsp-clients-svlangserver-mustIncludeIndexing . ["**/*.{sv,svh}"])
+     (lsp-clients-svlangserver-excludeIndexing . ["**/build/**/*.{sv,svh}"])
+     (lsp-clients-svlangserver-launchConfiguration . "verilator -sv --lint-only -Wall --timing")
+     (lsp-clients-svlangserver-formatCommand . "verible-verilog-format --indentation_spaces=3"))))
